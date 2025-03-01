ab83537d8d074939461eef63f656431d789ec3fa
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-01 16:31:54 +0800

    6-8 programs update

---
+++ Program 6.py
@@ -0,0 +1,6 @@
+def exponent():
+    first_number = int(input("First number: "))
+    second_number = int(input("Second number: "))
+    print("Result: ", first_number ** second_number)
+
+exponent()
---
+++ Program 7.py
@@ -0,0 +1,15 @@
+
+def allsum():
+    first = int(input("First number: "))
+    second = int(input("Second number: "))
+    third = int(input("Third number: "))
+    fourth = int(input("Fourth number: "))
+    fifth = int(input("Fifth number: "))
+    sixth = int(input("Sixth number: "))
+    seventh = int(input("Seven number: "))
+    eighth = int(input("Eighth number: "))
+    ninth = int(input("Ninth number: "))
+    tenth = int(input("Tenth number: "))
+    print("Result: ", first+second+third+fourth+fifth+sixth+seventh+eighth+ninth+tenth)
+
+allsum()
