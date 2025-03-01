3544651a99ce4c87e4bb0764ca8a985f1ae92195
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-01 18:15:08 +0800

    6-8 programs update

--- Program 8.py
+++ Program 8.py
@@ -1,4 +1,11 @@
+counter = 0
+
 for i in range(10):
-    given = int(input("Enter a number:"))
+    given = int(input(f"Enter a number {i+1}: "))
     if given % 2 != 0:
+        counter += 1
+
+print("Odd Counter: ", counter)
+
+
 
