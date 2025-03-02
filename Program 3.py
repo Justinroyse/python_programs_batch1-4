ade95f8d09e4d28bfa73d0a41f721f9e38f6861a
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-03 05:36:32 +0800

    update

--- Program 3.py
+++ Program 3.py
@@ -1,6 +1,2 @@
-def sums():
-    first_number = int(input("Enter first number: "))
-    second_number = int(input("Enter second number: "))
-
-    print("Result: ", first_number + second_number)
-sums()
+program = [int(input(f"Enter number ({i+1}): ")) for i in range(2)]
+print(sum(program))
