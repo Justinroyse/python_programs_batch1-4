59e5813b82045a44b54a8c8c2a9d685016016476
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-03 05:32:02 +0800

    6-8 programs update

--- Program 1.py
+++ Program 1.py
@@ -1,10 +1,2 @@
-def compare():
-    first_number = int(input("Enter first number: "))
-    second_number = int(input("Enter second number: "))
-    if first_number > second_number:
-        print(f"{first_number} is greater than {second_number}")
-    elif first_number < second_number:
-        print(f"{second_number} is greater than {first_number}")
-    else:
-        print(f"{first_number} and {second_number} is equal")
-compare()
+program = [int(input(f"Enter number ({i + 1}): ")) for i in range(2)]
+print(max(program))
