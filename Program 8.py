12915753ea346de59acf393d5d0faf45d52e7062
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-03 19:00:07 +0800

    update

--- Program 8.py
+++ Program 8.py
@@ -1,9 +1,9 @@
 store = []
 counter = 0
 for i in range(10):
-    given = int(input(f"Enter number ({i+1}): "))
-    store.append(given)
-    if given % 2 != 0:
+    program = int(input(f"Enter number {i+1}: "))
+    store.append(program)
+    if program % 2 != 0:
         counter += 1
     else:
         pass
