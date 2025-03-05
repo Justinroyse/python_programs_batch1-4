3b65094769806df81a688fbe77ddfe8e83271825
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-05 10:47:32 +0800

    update

---
+++ Program 6.py
@@ -0,0 +1,8 @@
+def difference(given):
+    operation = given[0]
+    for item in given[1:]:
+        operation -= item
+    return operation
+
+program = [int(input( f"Enter number ({i+1}):  ")) for i in range(10)]
+print(difference(program))
