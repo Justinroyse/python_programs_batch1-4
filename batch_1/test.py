a6ee24e70c576fa417dc888151580ec7b3a30fa9
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-09 21:30:35 +0800

    remove unwanted random files

--- batch_1/test.py
+++
@@ -1,9 +0,0 @@
-numbers = []
-print("*****Sum Calculator*****")
-print("\n")
-
-for i in range(10):
-    store = (input(f"Enter number {i + 1}: "))
-    numbers.append(int(store))
-
-print(sum(numbers))
