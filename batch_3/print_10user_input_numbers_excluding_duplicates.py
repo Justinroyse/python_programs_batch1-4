fcb3e5c017d25b4b0d495c2541677a2543ca33af
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-10 23:58:22 +0800

    Fixed bug None by returning an empty string, subject to further revision

--- batch_3/print_10user_input_numbers_excluding_duplicates.py
+++ batch_3/print_10user_input_numbers_excluding_duplicates.py
@@ -4,8 +4,8 @@ def duplicate_checker(given):
     for items in given[0:]:
         if stored_items != items:
             print(items)
-
+    return " "
 #for loop inside a list used for user number inputs
 program = [int(input(f"Enter number ({i+1}): "))
-           for i in range(10)]
+           for i in range(0,10)]
 print(duplicate_checker(program))
