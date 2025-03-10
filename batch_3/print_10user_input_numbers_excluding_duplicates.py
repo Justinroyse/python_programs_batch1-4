09f9e445506889e077d6c8e05d37d29d13ad2e74
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-10 23:14:35 +0800

    First complete program working, None showing in the result output bug

--- batch_3/print_10user_input_numbers_excluding_duplicates.py
+++ batch_3/print_10user_input_numbers_excluding_duplicates.py
@@ -1,7 +1,7 @@
 #function that checks for duplicates and displays numbers that don't have duplicates
 def duplicate_checker(given):
     stored_items = given[0]
-    for items in given[1:]:
+    for items in given[0:]:
         if stored_items != items:
             print(items)
 
