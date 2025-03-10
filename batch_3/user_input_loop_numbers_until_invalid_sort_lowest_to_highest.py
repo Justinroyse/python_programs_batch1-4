f5345122c0d8bf1170a79267144a83d97fe0bf0e
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-11 01:43:33 +0800

    Finished Coding Pseudocodes Prog05, bug: result is None

--- batch_3/user_input_loop_numbers_until_invalid_sort_lowest_to_highest.py
+++ batch_3/user_input_loop_numbers_until_invalid_sort_lowest_to_highest.py
@@ -1,3 +1,16 @@
 #Create a list variable to store numbers
 stored_numbers = []
 #Create while loop for user inputs numbers and implement sort function
+while True:
+    try:
+        user_input = int(input("Enter number: "))
+        stored_numbers.append(user_input)
+    except ValueError:
+        print("Invalid input: Finishing the Loop and Sorting the Numbers...")
+        break
+
+if stored_numbers:
+    print(stored_numbers.sort(reverse=False))
+else:
+    print("No valid input detected")
+
