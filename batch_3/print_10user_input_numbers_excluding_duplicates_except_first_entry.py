9af03c983916e8be2c92875a7702bfe2020ba7d7
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-11 00:12:11 +0800

    Coded list for loop variable for user inputs

--- batch_3/print_10user_input_numbers_excluding_duplicates_except_first_entry.py
+++ batch_3/print_10user_input_numbers_excluding_duplicates_except_first_entry.py
@@ -1,2 +1,7 @@
-#Create function that checks for duplicates and only prints the first entry
-#Create a list for loop that will be used for user_inputs
+#Create a function that checks for duplicates and only prints the first entry
+
+
+#Create a list for loop variable that will be used for user_inputs
+program = [int(input(f"Enter number ({inputs+1}): "))
+           for inputs in range(10)]
+
