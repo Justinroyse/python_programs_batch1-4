35254ea64de487107d621f689d609a4d0fe61ba2
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-11 00:39:55 +0800

    Working version Prog02 Batch 3

--- batch_3/print_10user_input_numbers_excluding_duplicates_except_first_entry.py
+++ batch_3/print_10user_input_numbers_excluding_duplicates_except_first_entry.py
@@ -1,7 +1,16 @@
 #Create a function that checks for duplicates and only prints the first entry
-
+def duplicate_checker(given):
+    seen = set()
+    unique_numbers = []
+    for number in given:
+        if number not in seen:
+            unique_numbers.append(number)
+            seen.add(number)
+    return unique_numbers
 
 #Create a list for loop variable that will be used for user_inputs
 program = [int(input(f"Enter number ({inputs+1}): "))
            for inputs in range(10)]
+
+print(duplicate_checker(program))
 
