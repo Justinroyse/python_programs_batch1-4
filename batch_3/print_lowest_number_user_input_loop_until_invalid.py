0cedff9b885bec461724caaa08d5ed0943d88eca
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-11 01:24:26 +0800

    Coded the while loop, finished the program without bugs

--- batch_3/print_lowest_number_user_input_loop_until_invalid.py
+++ batch_3/print_lowest_number_user_input_loop_until_invalid.py
@@ -1,3 +1,15 @@
 #Create list variable to store user input and reference the lowest number
 stored_numbers = []
 #Create while loop for user input until invalid and print the lowest number
+while True:
+    try:
+        user_input = int(input("Enter number: "))
+        stored_numbers.append(user_input)
+    except ValueError:
+        print("Finished Loop: Printing the Lowest Number")
+        break
+
+if stored_numbers:
+    print(f"Lowest number: {min(stored_numbers)}")
+else:
+    print("No valid input detected")
