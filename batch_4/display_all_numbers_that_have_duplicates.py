b75a832c9be5655f8b495b569b30467f43a0c828
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-12 03:34:07 +0800

    Fixed: implemented set, changed the logic from accesing list Prog01 Batch 4

--- batch_4/display_all_numbers_that_have_duplicates.py
+++ batch_4/display_all_numbers_that_have_duplicates.py
@@ -1,12 +1,16 @@
 #create function to check for duplicates
 def duplicate_checker(given):
-    checker = given[0]
-    for items in given[1:]:
-        if checker != given[1:]:
-            print(items)
-    return given
+    duplicates_numbers = []
+    seen = set()
+
+    for numbers in given:
+        if numbers in seen and numbers not in duplicates_numbers:
+            duplicates_numbers.append(numbers)
+        else:
+            seen.add(numbers)
+    return duplicates_numbers
 #create user input for loop 10 numbers
 program = [int(input(f"Enter number ({user_inputs+1}): "))
            for user_inputs in range(10)]
 #print result
-print(duplicate_checker(program))
+print("With Duplicates: ", duplicate_checker(program))
