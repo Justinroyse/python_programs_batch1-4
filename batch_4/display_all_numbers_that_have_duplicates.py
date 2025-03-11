b064dfa96dbc76c93271c19deed1ea0ccf5bfb85
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-12 02:57:50 +0800

    Coded Prog01, displays user inputs, bug: doesn't display duplicates only

--- batch_4/display_all_numbers_that_have_duplicates.py
+++ batch_4/display_all_numbers_that_have_duplicates.py
@@ -1,3 +1,12 @@
 #create function to check for duplicates
+def duplicate_checker(given):
+    checker = given[0]
+    for items in given[1:]:
+        if checker != given[1:]:
+            print(items)
+    return given
 #create user input for loop 10 numbers
+program = [int(input(f"Enter number ({user_inputs+1}): "))
+           for user_inputs in range(10)]
 #print result
+print(duplicate_checker(program))
