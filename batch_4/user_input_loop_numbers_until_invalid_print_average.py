da391fef293df1ec7ed3ea9f3cf72cdfe6cb44f0
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-12 13:40:36 +0800

    Coded Prog05, up for testing

--- batch_4/user_input_loop_numbers_until_invalid_print_average.py
+++ batch_4/user_input_loop_numbers_until_invalid_print_average.py
@@ -1,3 +1,22 @@
 #create function store list, display average
+def average(program):
+    collection_numbers = []
+
+    for numbers in program:
+        collection_numbers.append(numbers)
+
+    result = sum(collection_numbers)/len(collection_numbers)
+
+    return result
+
 #create user input while loop until input is invalid
+user_input_collection = []
+while True:
+    try:
+        user_input = int(input("Enter number: "))
+        user_input_collection.append(user_input)
+    except ValueError:
+        print("Invalid input, printing average...")
+        break
 #print result
+average(user_input_collection)
