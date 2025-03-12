c6f3536dc957e6d41046d7422fdc307b76ac3b3a
Justin Royse L. Solomon <92248392+Justinroyse@users.noreply.github.com>
2025-03-12 03:35:54 +0000

    function prototype Prog02 Batch 4

--- batch_4/display_number_with_most_duplicate.py
+++ batch_4/display_number_with_most_duplicate.py
@@ -1,3 +1,17 @@
 #create function to check for duplicates, display duplicate with most number
+def duplicate_checker(given):
+    duplicates = []
+    seen = set()
+
+    for numbers in given:
+        if numbers in seen and numbers not in duplicates:
+            duplicates.append(numbers)
+        else:
+            seen.add(numbers)
+	
+    for numbers in duplicates:
+        highest_checker = duplicates[0]
+        highest_counter = 0
+        
 #create while loop user input until invalid
 #print result
