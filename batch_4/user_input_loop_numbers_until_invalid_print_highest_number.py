2716d3d1fb980adb4f68a4e5a3d42e26dd415cf1
Justin Solomon <justinroyselsolomon@gmail.com>
2025-03-12 14:06:36 +0800

    Finished Prog03, pending for further optimization

--- batch_4/user_input_loop_numbers_until_invalid_print_highest_number.py
+++ batch_4/user_input_loop_numbers_until_invalid_print_highest_number.py
@@ -1,3 +1,14 @@
 #create function store list, identify highest number
+def highest(program):
+    return max(program)
 #create while loop for input until invalid
+user_input_collection = []
+while True:
+    try:
+        user_input = int(input("Enter number: "))
+        user_input_collection.append(user_input)
+    except ValueError:
+        print("Invalid input, printing highest number...")
+        break
 #print result
+print(highest(user_input_collection))
--- batch_4/user_input_loop_numbers_until_invalid_print_highest_to_lowest.py
+++ batch_4/user_input_loop_numbers_until_invalid_print_highest_to_lowest.py
@@ -1,3 +1,14 @@
 #create function store list to display highest to lowest user input number
+
 #create while loop for user input until invalid number
+user_input_collection = []
+while True:
+    try:
+        user_input = int(input("Enter number: "))
+        user_input_collection.append(user_input)
+    except ValueError:
+        print("Invalid input, sorting numbers... (Highest to lowest)")
+        break
+
+
 #print result
